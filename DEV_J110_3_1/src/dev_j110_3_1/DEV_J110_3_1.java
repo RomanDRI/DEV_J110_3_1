566f55992bbd8508667e8ceeb2e6a5148d42c758
Роман <@Asus>
2022-10-27 19:48:34 +0300

    Hello

---
+++ DEV_J110_3_1/src/dev_j110_3_1/DEV_J110_3_1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package dev_j110_3_1;
+
+/**
+ *
+ * @author Роман
+ */
+public class DEV_J110_3_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
